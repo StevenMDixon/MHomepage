feae9f2c54802549fcf9ccbfd79e697e5b9c73e9
Steven Dixon <stevendixon1128@gmail.com>
2018-06-14 15:46:17 +0000

    added route in server file

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
+
+app.get('/', function(req, res){
+    res.sendfile('index.html', { root: __dirname + "./dist/" } );
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
