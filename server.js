05db5985844056635261c969b9a84f32266f331f
Steven Dixon <stevendixon1128@gmail.com>
2018-06-14 16:26:31 +0000

    fixed routing on server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
+var app = express();
 var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
+var public = path.join(__dirname, 'dist');
 
-app.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname + '/dist/index.html'));
+app.use('/', express.static(public));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
 });
 
+app.get('/main.js', function(req, res, next) {  
+    res.sendfile('main.js'); 
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port);
